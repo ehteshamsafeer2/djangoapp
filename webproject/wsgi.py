95b69d399f1753eb1163f4d2ac84c67da60b5e76
Muhammad Ehtesham Safeer <107059374+ehteshamsafeer2@users.noreply.github.com>
2023-09-12 04:44:31 -0700

    Update wsgi.py

--- webproject/wsgi.py
+++ webproject/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 import os
-import django-heroku
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webproject.settings')
 
 application = get_wsgi_application()
-django_heroku.settings(locals())
+
